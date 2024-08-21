79c10005a3bce84bd2826135ca586fbfaf30bac5
Danninon <danninon@gmail.com>
2024-08-21 13:13:25 +0300

    Added test case for image,title,description

--- backend/__tests__/valid-url.test.ts
+++ backend/__tests__/valid-url.test.ts
@@ -25,6 +25,36 @@ describe('URL Validation Middleware', () => {
         expect(response.text).toBe('[{\"url\":\"https://example.com\",\"title\":\"Example Domain\",\"description\":\"No description found\",\"image\":\"No image found\"}]');
     });
 
+    it('should accept multiple valid URLs Texts, yes title, no image, no description', async () => {
+        const response = await request(app)
+            .post('/fetch-metadata')
+            .send({ urls: ["https://example.com", "https://express-rate-limit.mintlify.app/quickstart/usage"] }) // Multiple valid URLs
+            .set('Accept', 'application/json');
+
+        expect(response.status).toBe(200);
+        expect(response.text).toBe('' +
+            "[{\"url\":\"https://example.com\",\"title\":\"Example Domain\",\"description\":\"No description found\",\"image\":\"No image found\"},{\"url\":\"https://express-rate-limit.mintlify.app/quickstart/usage\",\"title\":\"Usage - express-rate-limit\",\"description\":\"No description found\",\"image\":\"No image found\"}]"
+        );
+    });
+
+    it('should accept images, title, description', async () => {
+        const response = await request(app)
+            .post('/fetch-metadata')
+            .send({ urls: ["https://github.com/danninon/metafetch/tree/main/backend"] }) // Multiple valid URLs
+            .set('Accept', 'application/json');
+
+        expect(response.status).toBe(200);
+        expect(response.text).toBe('' +
+            "[{\"url\":\"https://github.com/danninon/metafetch/tree/main/backend\",\"title\":\"metafetch/backend at main · danninon/metafetch · GitHub\",\"description\":\"Contribute to danninon/metafetch development by creating an account on GitHub.\",\"image\":\"https://opengraph.githubassets.com/ff81c2fff7d36b583b9681143503f49182ef2ce67b11a5262446e9870918112e/danninon/metafetch\"}]"
+        );
+    });
+    //     {
+    //         "url": "https://github.com/danninon/metafetch/tree/main/backend",
+    //         "title": "metafetch/backend at main · danninon/metafetch · GitHub",
+    //         "description": "Contribute to danninon/metafetch development by creating an account on GitHub.",
+    //         "image": "https://opengraph.githubassets.com/ff81c2fff7d36b583b9681143503f49182ef2ce67b11a5262446e9870918112e/danninon/metafetch"
+    //     }
+
     it('should reject an invalid URL with missing protocol', async () => {
         const response = await request(app)
             .post('/fetch-metadata')
@@ -56,6 +86,7 @@ describe('URL Validation Middleware', () => {
         expect(response.body.error).toBe('Invalid URL format: https://example@.com');
     });
 
+
     it('should reject an invalid URL with no domain', async () => {
         const response = await request(app)
             .post('/fetch-metadata')
@@ -66,17 +97,7 @@ describe('URL Validation Middleware', () => {
         expect(response.body.error).toBe('Invalid URL format: https://');
     });
 
-    it('should accept multiple valid URLs Texts, yes title, no image, no description', async () => {
-        const response = await request(app)
-            .post('/fetch-metadata')
-            .send({ urls: ["https://example.com", "https://express-rate-limit.mintlify.app/quickstart/usage"] }) // Multiple valid URLs
-            .set('Accept', 'application/json');
 
-        expect(response.status).toBe(200);
-        expect(response.text).toBe('' +
-            "[{\"url\":\"https://example.com\",\"title\":\"Example Domain\",\"description\":\"No description found\",\"image\":\"No image found\"},{\"url\":\"https://express-rate-limit.mintlify.app/quickstart/usage\",\"title\":\"Usage - express-rate-limit\",\"description\":\"No description found\",\"image\":\"No image found\"}]"
-        );
-    });
 
     it('should reject if one of the multiple URLs is invalid', async () => {
         const response = await request(app)
